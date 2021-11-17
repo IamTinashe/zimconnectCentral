122d3b5959d4902c00e1425052f861169c1a911d
Tinashe Zvihwati <tinashetp@gmail.com>
2021-11-17 12:44:28 +0200

    swagger_health

--- controllers/health.js
+++ controllers/health.js
@@ -4,16 +4,32 @@ const router = express.Router()
 
 /**
  * @swagger
+ * components:
+ *  schemas:
+ *   Health:
+ *    type: object 
+ *    properties:
+ *     healthy:
+ *      type: string
+ *      description: Server is healthy
+ */
+
+
+/**
+ * @swagger
  * /health:
- *   get:
- *     tags:
- *       - Health
- *     description: Checks if server is up
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: Healthy
+ *  get:
+ *   tags:
+ *    - Health
+ *   description: Returns the health of the server
+ *   produces:
+ *    - application/json
+ *   responses:
+ *    200:
+ *     description: Server is healthy
+ *     schema:
+ *      type: object
+ *      $ref: '#/components/schemas/Health'
  */
 router.get('/health', async (req, res) => {
   return res.status(200).json({ 'healthy': true });
