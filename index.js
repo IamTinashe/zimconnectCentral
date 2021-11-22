bdf5ee16ed66cb05a02b62fb643e2aa7704f271f
Tinashe Zvihwati <tinashetp@gmail.com>
2021-11-22 18:36:46 +0200

    adds_headers

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const Controllers = require('./controllers');
 
 const app = express();
 const port = 3000;
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
 
 app.use('/', Controllers);
 
